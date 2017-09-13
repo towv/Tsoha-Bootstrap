0444f8d4280101a7c51afe7a6cd86788a88998c3
Tommi Viiala <twviiala@gmail.com>
2017-09-13 23:55:45 +0300

    dokumentaation paivitys

--- app/controllers/hello_world_controller.php
+++ app/controllers/hello_world_controller.php
@@ -4,11 +4,29 @@
 
     public static function index(){
       // make-metodi renderöi app/views-kansiossa sijaitsevia tiedostoja
-   	  View::make('home.html');
+   	  //View::make('home.html');
+      echo 'Tämä on etusivu!';
     }
 
     public static function sandbox(){
       // Testaa koodiasi täällä
-      echo 'Hello World!';
+      //echo 'Hello World!';
+      View::make('helloworld.html');
+    }
+
+    public static function course_show(){
+    View::make('suunnitelmat/course_show.html');
+    }
+
+    public static function records_player(){
+    View::make('suunnitelmat/records_player.html');
+    }
+
+    public static function edit_course_show(){
+      View::make('suunnitelmat/edit_course_show.html');
+    }
+
+    public static function login(){
+      View::make('suunnitelmat/login.html');
     }
   }
--- config/routes.php
+++ config/routes.php
@@ -7,3 +7,22 @@
   $routes->get('/hiekkalaatikko', function() {
     HelloWorldController::sandbox();
   });
+
+  $routes->get('/records', function() {
+  HelloWorldController::records_player();
+});
+$routes->get('/records/1', function() {
+  HelloWorldController::course_show();
+});
+
+$routes->get('/records/1/1', function() {
+  HelloWorldController::edit_course_show();
+});
+
+$routes->get('/login', function() {
+  HelloWorldController::login();
+});
+
+$routes->get('/login', function() {
+  HelloWorldController::login();
+});
